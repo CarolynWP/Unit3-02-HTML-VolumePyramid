1e96f04491638ec7c4395e6a5998961e53785302
Carolyn Webster-Pless <99274537+CarolynWP@users.noreply.github.com>
2022-04-12 23:51:28 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 'use strict'
 
-function calculate(){
-	
+function calculate() {
+	// the variables
+	let base = parseFloat(document.getElementById('base').value);
+	let height = parseFloat(document.getElementById('height1').value);
+	let height2 = parseFloat(document.getElementById('height2').value);
+
+	//the calculations
+	let volume= (1/6)*base*height*height2
+
+	//the output
+	document.getElementById('volume').innerHTML = 'the volume of your right triangular pyramid is ' + volume.toFixed(2) + 'cm³'
 }
